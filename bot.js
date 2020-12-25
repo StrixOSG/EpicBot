d8dddc0e3a8216630ceed399a7baaab78861adff
StrixOSG <6631874+StrixOSG@users.noreply.github.com>
2020-12-25 15:29:06 -0600

    feat: typing epic good in chat returns the epic game that's free currently

--- bot.js
+++ bot.js
@@ -1,11 +1,47 @@
 require("dotenv").config();
+var epicStoreApi = require("./epicStoreApi");
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.login(process.env.BOT_TOKEN);
-client.on('ready', discordReady);
+client.on('ready', onDiscordReady);
+client.on("message", onDiscordMessage);
 
-function discordReady() {
+function onDiscordReady() {
     console.log('I\'m alive');
 }
+
+function onDiscordMessage(msg) {
+    if(msg.content.includes('Epic good')) {
+        fetchFreeGames(msg.channel);
+    }
+}
+
+const sendFreeGamesMessage = (gameData, channel) => {
+    console.log(gameData);
+	if (gameData.length === 0) {
+		// The empty game array means the data fetching failed
+		channel.send("The bot is offline maybe a maintainace 🤒");
+		return;
+	}
+	gameData.map((game) => {
+		const embed = {
+			title: game.title,
+			thumbnail: { url: game.image },
+			description: `Offer Ends: ${new Date(game.offerTill).toLocaleString()}`,
+			url: `https://www.epicgames.com/store/en-US/product/${game.productSlug}`,
+        };
+        channel.send('@here New Free Game on Epic Today');
+		channel.send({ embed: embed });
+	});
+}
+
+const fetchFreeGames = async (channel) => {
+	gameData = await epicStoreApi.getFreeGames();
+	if (gameData.error) {
+		console.log(data.error);
+		return;
+	}
+	sendFreeGamesMessage(gameData.freeGamesData, channel);
+};
---
+++ epicStoreApi.js
@@ -0,0 +1,40 @@
+const fetch = require("node-fetch");;
+
+module.exports = {
+    async getFreeGames() {
+        const url = "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=IN&allowCountries=IN";
+        let freeGamesData = [];
+        let error;
+        try {
+            const freeGamesResp = await fetch(url);
+            const freeGamesJson = await freeGamesResp.json();
+            for (let i = 1; i < freeGamesJson.data.Catalog.searchStore.elements.length; i++) {
+                obj = freeGamesJson.data.Catalog.searchStore.elements[i];
+                console.log(obj);
+                const imageUrl = obj.keyImages.reduce((acc, cur) => {
+                    if (cur.type === "DieselStoreFrontWide") {
+                        return cur;
+                    } else {
+                        return acc;
+                    }
+                }).url;
+
+                if (obj.promotions.promotionalOffers.length !== 0) {
+                    const game = {
+                        title: obj.title,
+                        offerTill: new Date(
+                            obj.promotions.promotionalOffers[0].promotionalOffers[0].endDate
+                        ),
+                        image: imageUrl,
+                        productSlug: obj.productSlug,
+                    };
+                    freeGamesData.push(game);
+                }
+            }
+        } catch (e) {
+            error = e;
+        }
+
+        return { freeGamesData , error };
+    },
+};
